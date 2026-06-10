058549cef6466038fb5b5c7d83fc0122272c3047
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2026-06-10 13:18:48 +0530

    Add files via upload

---
+++ frame-measurements.js
@@ -0,0 +1,52 @@
+const FRAME_MEASUREMENTS = {
+  "horizontal/10x10/1inch/frame1.png": { top: 236, left: 190, width: 2610, height: 2528 },
+  "horizontal/10x10/1inch/frame2.png": { top: 238, left: 354, width: 2300, height: 2530 },
+  "horizontal/10x12/1inch/frame1.png": { top: 354, left: 262, width: 3053, height: 2300 },
+  "horizontal/10x12/1inch/frame2.png": { top: 308, left: 286, width: 2975, height: 2306 },
+  "horizontal/10x12/1inch/frame3.png": { top: 234, left: 215, width: 3142, height: 2516 },
+
+  "horizontal/6x4/0.5inch/frame1.png": { top: 138, left: 143, width: 1517, height: 921 },
+  "horizontal/6x4/0.5inch/frame2.png": { top: 102, left: 114, width: 1569, height: 999 },
+  "horizontal/6x4/0.5inch/frame3.png": { top: 135, left: 147, width: 1497, height: 915 },
+  "horizontal/6x4/0.5inch/frame4.png": { top: 141, left: 140, width: 1517, height: 921 },
+  "horizontal/6x4/0.5inch/frame5.png": { top: 132, left: 132, width: 1545, height: 930 },
+  "horizontal/6x4/0.5inch/frame6.png": { top: 131, left: 152, width: 1502, height: 923 },
+
+  "horizontal/6x4/1inch/frame1.png": { top: 263, left: 244, width: 3138, height: 1909 },
+  "horizontal/6x4/1inch/frame2.png": { top: 228, left: 250, width: 3101, height: 1907 },
+  "horizontal/6x4/1inch/frame3.png": { top: 253, left: 236, width: 3092, height: 1874 },
+  "horizontal/6x4/1inch/frame4.png": { top: 204, left: 242, width: 3124, height: 1954 },
+  "horizontal/6x4/1inch/frame5.png": { top: 283, left: 293, width: 3034, height: 1845 },
+  "horizontal/6x4/1inch/frame6.png": { top: 0, left: 0, width: 3342, height: 2131 },
+  "horizontal/6x4/1inch/frame7.png": { top: 222, left: 266, width: 3073, height: 1951 },
+
+  "horizontal/8x10/0.5inch/frame1.png": { top: 211, left: 199, width: 2609, height: 1982 },
+  "horizontal/8x10/0.5inch/frame2.png": { top: 283, left: 233, width: 2529, height: 1840 },
+  "horizontal/8x10/0.5inch/frame3.png": { top: 263, left: 227, width: 2496, height: 1825 },
+
+  "vertical/10x10/1inch/frame1.png": { top: 200, left: 236, width: 2528, height: 2610 },
+  "vertical/10x10/1inch/frame2.png": { top: 238, left: 354, width: 2300, height: 2530 },
+
+  "vertical/10x12/1inch/frame1.png": { top: 285, left: 354, width: 2300, height: 3053 },
+  "vertical/10x12/1inch/frame2.png": { top: 339, left: 308, width: 2306, height: 2975 },
+  "vertical/10x12/1inch/frame3.png": { top: 243, left: 234, width: 2516, height: 3142 },
+
+  "vertical/6x4/0.5inch/frame1.png": { top: 143, left: 141, width: 921, height: 1517 },
+  "vertical/6x4/0.5inch/frame2.png": { top: 117, left: 102, width: 999, height: 1569 },
+  "vertical/6x4/0.5inch/frame3.png": { top: 156, left: 135, width: 915, height: 1497 },
+  "vertical/6x4/0.5inch/frame4.png": { top: 143, left: 141, width: 921, height: 1517 },
+  "vertical/6x4/0.5inch/frame5.png": { top: 123, left: 132, width: 930, height: 1545 },
+  "vertical/6x4/0.5inch/frame6.png": { top: 146, left: 131, width: 923, height: 1502 },
+
+  "vertical/6x4/1inch/frame1.png": { top: 218, left: 263, width: 1909, height: 3138 },
+  "vertical/6x4/1inch/frame2.png": { top: 249, left: 228, width: 1907, height: 3101 },
+  "vertical/6x4/1inch/frame3.png": { top: 272, left: 253, width: 1874, height: 3092 },
+  "vertical/6x4/1inch/frame4.png": { top: 234, left: 204, width: 1954, height: 3124 },
+  "vertical/6x4/1inch/frame5.png": { top: 273, left: 283, width: 1845, height: 3034 },
+  "vertical/6x4/1inch/frame6.png": { top: 258, left: 0, width: 2131, height: 3342 },
+  "vertical/6x4/1inch/frame7.png": { top: 266, left: 227, width: 1951, height: 3073 },
+
+  "vertical/8x10/0.5inch/frame1.png": { top: 192, left: 211, width: 1982, height: 2609 },
+  "vertical/8x10/0.5inch/frame2.png": { top: 238, left: 283, width: 1840, height: 2529 },
+  "vertical/8x10/0.5inch/frame3.png": { top: 277, left: 263, width: 1825, height: 2496 }
+};
