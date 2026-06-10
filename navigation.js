33d5150ed3bd9ad9a214059e0aabdd8da0d97714
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2026-06-10 17:16:19 +0530

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,106 @@
+function createNavigation(currentStep) {
+
+    const steps = [
+
+        {
+            name: "Frame Size",
+            link: "../index.html"
+        },
+
+        {
+            name: "Size",
+            link: "../photo_size/index.html"
+        },
+
+        {
+            name: "Orientation",
+            link: "../orientation/index.html"
+        },
+
+        {
+            name: "Thickness",
+            link: "../frame-thickness/index.html"
+        },
+
+        {
+            name: "Frame",
+            link: "../frame-selection/index.html"
+        },
+
+        {
+            name: "Preview",
+            link: "preview"
+        },
+
+        {
+            name: "Wall",
+            link: "../wall-selection/index.html"
+        },
+
+        {
+            name: "Final",
+            link: "../wall-preview/index.html"
+        }
+
+    ];
+
+    const nav = document.getElementById("wizardNav");
+
+    steps.forEach((step, index) => {
+
+        const a = document.createElement("a");
+
+        a.innerText = step.name;
+
+        a.classList.add("wizard-step");
+
+        if (index < currentStep) {
+
+            a.classList.add("completed");
+
+        }
+
+        else if (index === currentStep) {
+
+            a.classList.add("active");
+
+        }
+
+        else {
+
+            a.classList.add("disabled");
+
+        }
+
+        if (step.link === "preview") {
+
+            const orientation =
+                localStorage.getItem("orientation");
+
+            if (orientation === "vertical") {
+
+                a.href = "../vertical/index.html";
+
+            } else {
+
+                a.href = "../horizontal/index.html";
+
+            }
+
+        } else {
+
+            a.href = step.link;
+
+        }
+
+        if (index > currentStep) {
+
+            a.href = "#";
+
+        }
+
+        nav.appendChild(a);
+
+    });
+
+}
