e6c3dcdb112e1bd020e5ee21b59d27968fed36e1
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2026-06-11 14:53:26 +0530

    Add new frame measurements for multiple sizes
    Added new frame measurements for various sizes and orientations.

--- frame-measurements.js
+++ frame-measurements.js
@@ -48,5 +48,114 @@ const FRAME_MEASUREMENTS = {
 
   "vertical/8x10/0.5inch/frame1.png": { top: 192, left: 211, width: 1982, height: 2609 },
   "vertical/8x10/0.5inch/frame2.png": { top: 238, left: 283, width: 1840, height: 2529 },
-  "vertical/8x10/0.5inch/frame3.png": { top: 277, left: 263, width: 1825, height: 2496 }
+  "vertical/8x10/0.5inch/frame3.png": { top: 277, left: 263, width: 1825, height: 2496 },
+  // HORIZONTAL
+
+"horizontal/16x20/1.5inch/frame1.png": { top: 304, left: 327, width: 5335, height: 4178 },
+"horizontal/16x20/1.5inch/frame2.png": { top: 377, left: 386, width: 5249, height: 4069 },
+
+"horizontal/16x20/2inch/frame1.png": { top: 346, left: 405, width: 5217, height: 4110 },
+
+"horizontal/16x24/1.5inch/frame1.png": { top: 464, left: 475, width: 8032, height: 5087 },
+"horizontal/16x24/1.5inch/frame2.png": { top: 340, left: 371, width: 6450, height: 4109 },
+"horizontal/16x24/1.5inch/frame3.png": { top: 346, left: 370, width: 6505, height: 4112 },
+
+"horizontal/16x24/2inch/frame1.png": { top: 352, left: 427, width: 8192, height: 5239 },
+
+"horizontal/20x24/1.5inch/frame1.png": { top: 474, left: 435, width: 6327, height: 5102 },
+"horizontal/20x24/1.5inch/frame2.png": { top: 352, left: 378, width: 6421, height: 5262 },
+
+"horizontal/20x24/2inch/frame1.png": { top: 417, left: 473, width: 6283, height: 5167 },
+
+"horizontal/20x30/1.5inch/20x30-02.png": { top: 271, left: 288, width: 3427, height: 5420 },
+"horizontal/20x30/1.5inch/frame1.png": { top: 464, left: 476, width: 8031, height: 5086 },
+"horizontal/20x30/1.5inch/frame2.png": { top: 339, left: 370, width: 6452, height: 4111 },
+"horizontal/20x30/1.5inch/frame3.png": { top: 285, left: 271, width: 5420, height: 3427 },
+
+"horizontal/20x30/2inch/frame1.png": { top: 352, left: 428, width: 8191, height: 5239 },
+
+// HORIZONTAL 30x40 — 1.5 inch
+
+"horizontal/30x40/1.5inch/frame1.png": {
+    top: 469,
+    left: 462,
+    width: 7093,
+    height: 5078
+},
+
+"horizontal/30x40/1.5inch/frame2.png": {
+    top: 469,
+    left: 462,
+    width: 7093,
+    height: 5078
+},
+
+// HORIZONTAL 30x40 — 2 inch
+
+"horizontal/30x40/2inch/frame1.png": {
+    top: 353,
+    left: 360,
+    width: 7318,
+    height: 5259
+},
+
+"horizontal/30x40/2inch/frame2.png": {
+    top: 411,
+    left: 498,
+    width: 7003,
+    height: 5240
+},
+
+
+// VERTICAL
+
+"vertical/16x20/1.5inch/frame1.png": { top: 338, left: 304, width: 4178, height: 5335 },
+"vertical/16x20/1.5inch/frame2.png": { top: 365, left: 377, width: 4069, height: 5249 },
+
+"vertical/16x20/2inch/frame1.png": { top: 378, left: 346, width: 4110, height: 5217 },
+
+"vertical/16x24/1.5inch/frame1.png": { top: 493, left: 464, width: 5087, height: 8032 },
+"vertical/16x24/1.5inch/frame2.png": { top: 379, left: 340, width: 4109, height: 6450 },
+"vertical/16x24/1.5inch/frame3.png": { top: 325, left: 346, width: 4112, height: 6505 },
+
+"vertical/16x24/2inch/frame1.png": { top: 381, left: 352, width: 5239, height: 8192 },
+
+"vertical/20x24/1.5inch/frame1.png": { top: 438, left: 474, width: 5102, height: 6327 },
+"vertical/20x24/1.5inch/frame2.png": { top: 401, left: 352, width: 5262, height: 6421 },
+
+"vertical/20x24/2inch/frame1.png": { top: 444, left: 417, width: 5167, height: 6283 },
+
+"vertical/20x30/1.5inch/20x30-02.png": { top: 271, left: 288, width: 3427, height: 5420 },
+"vertical/20x30/1.5inch/frame1.png": { top: 493, left: 464, width: 5086, height: 8031 },
+"vertical/20x30/1.5inch/frame2.png": { top: 378, left: 339, width: 4111, height: 6452 },
+
+"vertical/20x30/2inch/frame1.png": { top: 381, left: 352, width: 5239, height: 8191 },
+
+"vertical/30x40/1.5inch/frame1.png": {
+    top: 520,
+    left: 500,
+    width: 5000,
+    height: 6900
+},
+
+"vertical/30x40/1.5inch/frame2.png": {
+    top: 450,
+    left: 470,
+    width: 5070,
+    height: 7090
+},
+
+"vertical/30x40/2inch/frame1.png": {
+    top: 322,
+    left: 353,
+    width: 5258,
+    height: 7317
+},
+
+"vertical/30x40/2inch/frame2.png": {
+    top: 499,
+    left: 411,
+    width: 5590,
+    height: 7002
+}
 };
